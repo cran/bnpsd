deabc0b4490b3aaa2929c29079f6bf3594386249
Alejandro Ochoa <ochoa@princeton.edu>
2018-01-16 09:07:50 +0000

    version 1.0.1

---
+++ R/q1d.R
@@ -0,0 +1,78 @@
+## TODO: add other q* scenarios (1D circular, 2D, etc)
+
+#' Construct admixture proportion matrix for 1D geography
+#'
+#' Assumes \eqn{k} intermediate subpopulations placed along a line at locations \eqn{1:k} spread by random walks, then \eqn{n} individuals sampled equally spaced in \eqn{[a,b]} (default \eqn{[0.5, k+0.5]}) draw their admixture proportions relative to the Normal density that models the random walks of each of these intermediate subpopulations.
+#' The spread of the random walks (the \eqn{\sigma} of the Normal densities) is set to \code{sigma} if not missing, otherwise \eqn{\sigma} is found numerically to give the desired bias coefficient \code{s}, the vector \code{F} of \eqn{F_{ST}}{FST}'s for the intermediate subpopulations up to a scalar factor, and the final \eqn{F_{ST}}{FST} of the admixed individuals (see details below).
+#'
+#' When \code{sigma} is missing, the function determines its value using the desired \code{s}, \code{F} up to a scalar factor, and \code{Fst}.
+#' Uniform weights for the final generalized \eqn{F_{ST}}{FST} are assumed.
+#' The scaling factor of the input \code{F} is irrelevant because it cancels out in \code{s}; after \code{sigma} is found, \code{F} is rescaled to give the desired final \eqn{F_{ST}}{FST}.
+#' However, the function stops with a fatal error if the rescaled \code{F} takes on any values greater than 1, which are not allowed since \code{F} are IBD probabilities.
+#'
+#' @param n Number of individuals
+#' @param k Number of intermediate subpopulations
+#' @param sigma Spread of intermediate subpopulations (standard deviation of normal densities)
+#' @param a Location of first individual
+#' @param b Location of last individual
+#'
+#' OPTIONS FOR BIAS COEFFICIENT VERSION
+#' 
+#' @param s The desired bias coefficient, which specifies \eqn{\sigma} indirectly.  Required if \code{sigma} is missing
+#' @param F The length-\eqn{k} vector of inbreeding coefficients (or \eqn{F_{ST}}{FST}'s) of the intermediate subpopulations, up to a scaling factor (which cancels out in calculations).  Required if \code{sigma} is missing
+#' @param Fst The desired final \eqn{F_{ST}}{FST} of the admixed individuals.  Required if \code{sigma} is missing
+#' @param interval Restrict the search space of \eqn{\sigma} to this interval
+#' @param tol The numerical tolerance used to declare the solution found
+#'
+#' @return If \code{sigma} was provided, the \eqn{n \times k}{n-by-k} admixture proportion matrix \eqn{Q}.  If \code{sigma} is missing, a named list is returned containing \code{Q}, the rescaled \code{F}, and the \code{sigma} that together give the desired \eqn{s} and final \eqn{F_{ST}}{FST} of the admixed individuals.
+#'
+#' @examples
+#' ## admixture matrix for 1000 individuals drawing alleles from 10 subpops
+#' ## and a spread of 2 standard deviations along the 1D geography
+#' Q <- q1d(n=1000, k=10, sigma=2)
+#'
+#' ## a similar model but with a bias coefficient "s" of exactly 1/2
+#' k <- 10
+#' F <- 1:k # Fst vector for intermediate subpops, up to a factor (will be rescaled below)
+#' Fst <- 0.1 # desired final Fst of admixed individuals
+#' obj <- q1d(n=1000, k=k, s=0.5, F=F, Fst=Fst)
+#' ## in this case return value is a named list with three items:
+#' Q <- obj$Q # admixture proportions
+#' F <- obj$F # rescaled Fst vector for intermediate subpops
+#' sigma <- obj$sigma # and the sigma that gives the desired s and final Fst
+#'
+#' @export
+q1d <- function(n, k, sigma, a=0.5, b=k+0.5, s, F, Fst, interval=c(0.1,10), tol=.Machine$double.eps) {
+    ## figure out if we need the more complicated algorithm...
+    sigmaMissing <- missing(sigma) # remember after it was set
+    if (sigmaMissing) { ## this triggers s version
+        if (missing(s)) stop('Fatal in q1d: s is required when sigma is missing!')
+        if (missing(F)) stop('Fatal in q1d: F is required when sigma is missing!')
+        if (missing(Fst)) stop('Fatal in q1d: Fst is required when sigma is missing!')
+        sigma <- biasCoeffSolveParam(s, F, n, q1d, interval, tol)
+    }
+    sigma2 <- -2*sigma^2 # square once for loop below (plus other Normal constants)
+    
+    ## the x-coordinates of the n individuals based on [a,b] limits
+    xs <- a + (0:(n-1))/(n-1)*(b-a)
+    ## and subpopulations (same deal, except fixed [1,k] range)
+    mus <- 1:k
+    
+    ## construct the coefficients of each person now!
+    Q <- matrix(nrow=n, ncol=k) # dimensions match that of makeQ
+    for (i in 1:n) {
+        ## collect the density values for each intermediate subpopulation at individual i's position
+        ## line implements super fast Normal without constant factors (which only involve constant sigma)
+        Q[i,] <- exp( (xs[i] - mus)^2 / sigma2 )
+    }
+    ## normalize to have rows/coefficients sum to 1!
+    Q <- Q/rowSums(Q) # return!
+
+    if (sigmaMissing) { ## this triggers s version
+        F <- rescaleF(Q, F, Fst) # let's rescale F now!
+        return( list(Q=Q, F=F, sigma=sigma) ) # return all this additional data!
+    } else {
+        return(Q) # in direct case, always return Q
+    }
+}
+
---
+++ tests/testthat/test_bnpsd.R
@@ -0,0 +1,334 @@
+context('bnpsd')
+
+## start with lower-level/internal tests, more informative that higher-level function errors
+
+test_that("coanc works in toy cases", {
+    Q <- diag(c(1,1)) # an IS model with two subpops
+    F <- c(0.1, 0.3)
+    ThetaExp <- diag(F) # the Theta we expect for this setup
+    Theta <- coanc(Q, F)
+    expect_equal(Theta, ThetaExp)
+
+    ## same Q, scalar F
+    F <- 0.2
+    ThetaExp <- diag(c(F,F)) # the Theta we expect for this setup
+    Theta <- coanc(Q, F)
+    expect_equal(Theta, ThetaExp)
+
+    ## same Q, matrix F
+    Fv <- c(0.1,0.4) # vector version
+    F <- diag(Fv) # matrix version
+    Theta <- coanc(Q, F)
+    expect_equal(Theta, F) # F is the theta we expect in this case
+
+    ## most complex case, just a general math check
+    Q <- matrix(c(0.7, 0.3, 0.2, 0.8), nrow=2, byrow=TRUE)
+    F <- matrix(c(0.3, 0.1, 0.1, 0.3), nrow=2, byrow=TRUE)
+    ThetaExp <- Q %*% F %*% t(Q) # the Theta we expect for this setup (slower but more explicit version)
+    Theta <- coanc(Q, F)
+    expect_equal(Theta, ThetaExp)
+}) 
+
+test_that("fst works in toy cases", {
+    Q <- diag(c(1,1)) # an IS model with two subpops
+    F <- c(0.1, 0.3)
+    fst1 <- mean(F) # the Theta we expect for this setup
+    fst2 <- fst(Q, F)
+    expect_equal(fst1, fst2)
+
+    ## same Q, scalar F
+    F <- 0.2
+    fst2 <- fst(Q, F)
+    expect_equal(F, fst2)
+
+    ## same Q, matrix F
+    Fv <- c(0.1,0.4) # vector version
+    F <- diag(Fv) # matrix version
+    fst1 <- mean(Fv)
+    fst2 <- fst(Q, F)
+    expect_equal(fst1, fst2) # F is the theta we expect in this case
+
+    ## most complex case, just a general math check
+    Q <- matrix(c(0.7, 0.3, 0.2, 0.8), nrow=2, byrow=TRUE)
+    F <- matrix(c(0.3, 0.1, 0.1, 0.3), nrow=2, byrow=TRUE)
+    fst1 <- mean(diag(Q %*% F %*% t(Q))) # the Fst we expect for this setup (slower but more explicit version)
+    fst2 <- fst(Q, F)
+    expect_equal(fst1, fst2)
+})
+
+test_that("biasCoeff agrees with explicitly calculated bias coeff s", {
+    n <- 5
+    k <- 2
+    sigma <- 1
+    Q <- q1d(n, k, sigma)
+    F <- 1:k # scale doesn't matter right now...
+    Theta <- coanc(Q, F) # in wrong scale but meh
+    sWant <- mean(Theta)/mean(diag(Theta)) # this is the correct bias coeff, with uniform weights
+    s <- biasCoeff(Q, F) # calculation to compare to
+    expect_equal(s, sWant)
+    expect_true(s > 0) # other obvious properties...
+    expect_true(s <= 1)
+
+    ## repeat with non-uniform weights...
+    w <- runif(n) # random weights for given number of individuals
+    w <- w/sum(w) # normalize to add up to 1! # NOTE: should check sum(w)!= 0, meh...
+    sWant <- drop(w %*% Theta %*% w)/drop( diag(Theta) %*% w ) # this is the correct bias coeff, with uniform weights
+    s <- biasCoeff(Q, F, w) # calculation to compare to
+    expect_equal(s, sWant)
+    expect_true(s > 0) # other obvious properties...
+    expect_true(s <= 1)
+})
+
+test_that("qis returns valid admixture coefficients", {
+    labs <- c(1,2,2,3,3,3,4,4,4,4)
+    n <- length(labs)
+    k <- length(unique(labs))
+    Q <- qis(labs)
+    ## general tests for admixture matrices
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+    ## specific tests for qis
+    expect_true(all(Q %in% c(TRUE,FALSE)))
+    expect_true(all(colnames(Q) == sort(unique(labs))))
+    
+    ## test with provided subpops
+    subpops <- 4:1
+    Q <- qis(labs, subpops)
+    ## general tests for admixture matrices
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+    ## specific tests for qis
+    expect_true(all(Q %in% c(TRUE,FALSE)))
+    expect_true(all(colnames(Q) == subpops))
+    
+    ## test with provided subpops (additional labels)
+    k <- 10
+    subpops <- 1:k
+    Q <- qis(labs, subpops)
+    ## general tests for admixture matrices
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+    ## specific tests for qis
+    expect_true(all(Q %in% c(TRUE,FALSE)))
+    expect_true(all(colnames(Q) == subpops))
+
+    ## test with provided subpops (missing labels, must die!)
+    subpops <- 1:3 # missing 4!
+    expect_error( qis(labs, subpops) )
+})
+
+test_that("q1d returns valid admixture coefficients", {
+    n <- 10
+    k <- 2
+    sigma <- 1
+    Q <- q1d(n, k, sigma)
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+
+    ## test s version
+    s <- 0.5
+    F <- 1:k # scale doesn't matter right now...
+    Fst <- 0.1
+    obj <- q1d(n, k, s=s, F=F, Fst=Fst)
+    Q <- obj$Q # returns many things in this case, get Q here
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+})
+
+test_that("q1dc returns valid admixture coefficients", {
+    n <- 10
+    k <- 2
+    sigma <- 1
+    Q <- q1dc(n, k, sigma)
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+
+    ## test s version
+    s <- 0.5
+    F <- 1:k # scale doesn't matter right now...
+    Fst <- 0.1
+    obj <- q1dc(n, k, s=s, F=F, Fst=Fst)
+    Q <- obj$Q # returns many things in this case, get Q here
+    expect_equal(nrow(Q), n) # n rows
+    expect_equal(ncol(Q), k) # k columns
+    expect_true(all(Q >= 0)) # all are non-negative
+    expect_true(all(Q <= 1)) # all are smaller or equal than 1
+    expect_equal(rowSums(Q), rep.int(1,n)) # rows sum to 1, vector length n
+})
+
+test_that("biasCoeffSolveParam agrees with reverse func", {
+    n <- 1000
+    F <- c(0.1,0.2,0.3)
+    k <- length(F)
+    sWant <- 0.5
+
+    ## test with q1d
+    sigma <- biasCoeffSolveParam(s=sWant, F=F, n=n, q1d) # get sigma
+    ## construct everything and verify s == sWant
+    Q <- q1d(n, k, sigma) # now get Q from there
+    Theta <- coanc(Q, F)
+    s <- mean(Theta)/mean(diag(Theta)) # this is the correct bias coeff, with uniform weights
+    expect_equal(s, sWant)
+    ## since we set 0<sWant<1, nothing else to test
+
+    ## test with q1dc
+    sigma <- biasCoeffSolveParam(s=sWant, F=F, n=n, q1dc) # get sigma
+    ## construct everything and verify s == sWant
+    Q <- q1dc(n, k, sigma) # now get Q from there
+    Theta <- coanc(Q, F)
+    s <- mean(Theta)/mean(diag(Theta)) # this is the correct bias coeff, with uniform weights
+    expect_equal(s, sWant)
+    ## since we set 0<sWant<1, nothing else to test
+})
+
+test_that("rescaleF agrees with explicitly Fst calculation", {
+    n <- 5
+    k <- 2
+    sigma <- 1
+    Fst <- 0.1
+    Q <- q1d(n, k, sigma)
+    F <- 1:k # scale doesn't matter right now...
+    F2 <- rescaleF(Q, F, Fst) # calculation to compare to
+    Theta <- coanc(Q, F2) # in wrong scale but meh
+    Fst2 <- mean(diag(Theta)) # this is the actual Fst, with uniform weights
+    expect_equal(Fst, Fst2)
+    # since 0 < Fst=0.1 < 1, there's nothing else to test
+})
+
+test_that("rpanc is in range", {
+    m <- 1000
+    pAnc <- rpanc(m)
+    expect_equal(length(pAnc), m)
+    expect_true(all(pAnc >= 0)) # all are non-negative
+    expect_true(all(pAnc <= 1)) # all are smaller or equal than 1
+})
+
+test_that("rpint is in range", {
+    m <- 1000
+    F <- c(0.1,0.2,0.3)
+    k <- length(F)
+    pAnc <- rpanc(m)
+    B <- rpint(pAnc, F)
+    expect_equal(nrow(B), m)
+    expect_equal(ncol(B), k)
+    expect_true(all(B >= 0)) # all are non-negative
+    expect_true(all(B <= 1)) # all are smaller or equal than 1
+})
+
+test_that("rpiaf is in range", {
+    m <- 1000
+    F <- c(0.1,0.2,0.3)
+    k <- length(F)
+    Q <- diag(rep.int(1,k)) # island model for test...
+    Q <- rbind(Q,Q,Q) # repeat so we have multiple people per island
+    n <- nrow(Q) # number of individuals (3*k)
+    pAnc <- rpanc(m)
+    B <- rpint(pAnc, F)
+    P <- rpiaf(B,Q)
+    expect_equal(nrow(P), m)
+    expect_equal(ncol(P), n)
+    expect_true(all(P >= 0)) # all are non-negative
+    expect_true(all(P <= 1)) # all are smaller or equal than 1
+})
+
+test_that("rgeno is in range", {
+    m <- 1000
+    F <- c(0.1,0.2,0.3)
+    k <- length(F)
+    Q <- diag(rep.int(1,k)) # island model for test...
+    Q <- rbind(Q,Q,Q) # repeat so we have multiple people per island
+    n <- nrow(Q) # number of individuals (3*k)
+    pAnc <- rpanc(m)
+    B <- rpint(pAnc, F)
+    P <- rpiaf(B,Q)
+    X <- rgeno(P) # direct test
+    expect_equal(nrow(X), m)
+    expect_equal(ncol(X), n)
+    expect_true(all(X %in% c(0,1,2))) # only three values allowed!
+    X <- rgeno(B,Q) # indirect draw test
+    expect_equal(nrow(X), m)
+    expect_equal(ncol(X), n)
+    expect_true(all(X %in% c(0,1,2))) # only three values allowed!
+    X <- rgeno(B,Q,lowMem=TRUE) # indirect draw test with low-mem algo
+    expect_equal(nrow(X), m)
+    expect_equal(ncol(X), n)
+    expect_true(all(X %in% c(0,1,2))) # only three values allowed!
+})
+
+test_that("rbnpsd works well", {
+    m <- 1000
+    F <- c(0.1,0.2,0.3)
+    k <- length(F)
+    Q <- diag(rep.int(1,k)) # island model for test...
+    Q <- rbind(Q,Q,Q) # repeat so we have multiple people per island
+    n <- nrow(Q) # number of individuals (3*k)
+    ## run rbnpsd
+    out <- rbnpsd(Q, F, m)
+    X <- out$X # genotypes
+    P <- out$P # IAFs
+    B <- out$B # Intermediate AFs
+    pAnc <- out$Pa # Ancestral AFs
+    ## test X
+    expect_equal(nrow(X), m)
+    expect_equal(ncol(X), n)
+    expect_true(all(X %in% c(0,1,2))) # only three values allowed!
+    ## test P
+    expect_equal(nrow(P), m)
+    expect_equal(ncol(P), n)
+    expect_true(all(P >= 0)) # all are non-negative
+    expect_true(all(P <= 1)) # all are smaller or equal than 1
+    ## test B
+    expect_equal(nrow(B), m)
+    expect_equal(ncol(B), k)
+    expect_true(all(B >= 0)) # all are non-negative
+    expect_true(all(B <= 1)) # all are smaller or equal than 1
+    ## test pAnc
+    expect_equal(length(pAnc), m)
+    expect_true(all(pAnc >= 0)) # all are non-negative
+    expect_true(all(pAnc <= 1)) # all are smaller or equal than 1
+})
+
+test_that("rbnpsd lowMem=TRUE works well", {
+    m <- 1000
+    F <- c(0.1,0.2,0.3)
+    k <- length(F)
+    Q <- diag(rep.int(1,k)) # island model for test...
+    Q <- rbind(Q,Q,Q) # repeat so we have multiple people per island
+    n <- nrow(Q) # number of individuals (3*k)
+    ## run rbnpsd
+    out <- rbnpsd(Q, F, m, lowMem=TRUE)
+    X <- out$X # genotypes
+    B <- out$B # Intermediate AFs
+    pAnc <- out$Pa # Ancestral AFs
+    ## test X
+    expect_equal(nrow(X), m)
+    expect_equal(ncol(X), n)
+    expect_true(all(X %in% c(0,1,2))) # only three values allowed!
+    ## test B
+    expect_equal(nrow(B), m)
+    expect_equal(ncol(B), k)
+    expect_true(all(B >= 0)) # all are non-negative
+    expect_true(all(B <= 1)) # all are smaller or equal than 1
+    ## test pAnc
+    expect_equal(length(pAnc), m)
+    expect_true(all(pAnc >= 0)) # all are non-negative
+    expect_true(all(pAnc <= 1)) # all are smaller or equal than 1
+})
